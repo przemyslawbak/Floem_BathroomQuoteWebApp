92d4457c25919f4e65df3c13cc19395906de5ed4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-12 23:37:05 +0200

    Changed program build options; Added gitignore file;

--- Floem_BathroomQuote/Program.cs
+++ Floem_BathroomQuote/Program.cs
@@ -1,11 +1,6 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Floem_BathroomQuote
 {
@@ -13,14 +8,16 @@ namespace Floem_BathroomQuote
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options =>
+                options.ValidateScopes = false)
+                .Build();
+        }
     }
 }
