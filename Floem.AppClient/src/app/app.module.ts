65e3092945d416fbd2a106468198b485f188a18e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-13 00:17:16 +0200

    Client app added;

---
+++ Floem.AppClient/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxSpinnerModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
