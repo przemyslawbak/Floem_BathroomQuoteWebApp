2042e962e842cb9216ac1934672142b3e645a394
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-12 23:42:42 +0200

    In spartup using endpoints and CORS;

--- Floem_BathroomQuote/Startup.cs
+++ Floem_BathroomQuote/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -31,13 +30,10 @@ namespace Floem_BathroomQuote
             }
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
         }
     }
