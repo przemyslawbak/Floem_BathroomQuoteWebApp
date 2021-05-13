eb16b125a1d153944fc947eabf82cd7c9f881ddd
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-13 18:22:07 +0200

    Removed router-outlet, maybe no need at all; TODO updated; Fixed /main rouing bug;

--- Floem_AppClient/src/app/app-routing.module.ts
+++ Floem_AppClient/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-
-const routes: Routes = [
-  {
-    path: 'main',
-    component: AppComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
