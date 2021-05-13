dfb277f64ffed9dfe8afc0a3480cf91d71d981af
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-13 11:08:12 +0200

    AppHeader added to the core module;

--- Floem_AppClient/src/app/app-core/app-core.module.ts
+++ Floem_AppClient/src/app/app-core/app-core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
 
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [FooterComponent],
-  exports: [FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  declarations: [FooterComponent, HeaderComponent],
+  exports: [FooterComponent, HeaderComponent],
   providers: [],
 })
 export class AppCoreModule {}
