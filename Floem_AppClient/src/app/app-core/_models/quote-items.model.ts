309a6a2dc466cb901eec50eb6da57fabc191789f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-18 15:35:16 +0200

    Re-planned step headers with pics and styling;

--- Floem_AppClient/src/app/app-core/_models/quote-items.model.ts
+++ Floem_AppClient/src/app/app-core/_models/quote-items.model.ts
@@ -50,7 +50,7 @@ export class QuoteItems {
     this.height = 200;
     this.depth = 100;
     this.units = DimentionUnits.Centimeters;
-    this.removals = true;
+    this.removals = false;
     this.floorLvt = false;
     this.floorOther = false;
     this.floorTiled = false;
