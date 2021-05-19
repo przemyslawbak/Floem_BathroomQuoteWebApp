7be2687a23626fc0fe5742191b812bd4ed2d0ebf
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-19 14:53:42 +0200

    Sample tile pics added; Testing tile display; TODO updated;

---
+++ Floem_AppClient/src/app/app-core/_models/unit-items.model.ts
@@ -0,0 +1,5 @@
+export interface UnitModel {
+  type: string;
+  code: string;
+  price: number;
+}
--- Floem_AppClient/src/app/app-core/form/form.component.ts
+++ Floem_AppClient/src/app/app-core/form/form.component.ts
@@ -6,6 +6,7 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { StepperOrientation } from '@angular/material/stepper';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UnitModel } from '../_models/unit-items.model';
 
 @Component({
   selector: 'app-form',
@@ -239,5 +240,41 @@ export class FormComponent {
 
   public setChangingDoors(): void {
     this.quoteModel.doorsChanging = true;
+  }
+
+  public getTiles(): UnitModel[] {
+    let tiles: UnitModel[] = [
+      {
+        type: 'tiles',
+        code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
+        price: 34.12,
+      } as UnitModel,
+      {
+        type: 'tiles',
+        code: '6de173e3-b387-4dee-bbe8-846390719e56',
+        price: 31.05,
+      } as UnitModel,
+      {
+        type: 'tiles',
+        code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
+        price: 17.25,
+      } as UnitModel,
+      {
+        type: 'tiles',
+        code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
+        price: 19.95,
+      } as UnitModel,
+      {
+        type: 'tiles',
+        code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
+        price: 45.05,
+      } as UnitModel,
+      {
+        type: 'tiles',
+        code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
+        price: 47.65,
+      } as UnitModel,
+    ];
+    return tiles;
   }
 }
