b26f5bae909c07d86304063cb7276b1fa702b370
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-22 21:33:05 +0200

    Fixed bug: what happens on itemListed click?; Removed linear stepper property and forms being completed;

--- Floem_AppClient/src/app/app-core/form/form.component.ts
+++ Floem_AppClient/src/app/app-core/form/form.component.ts
@@ -19,6 +19,162 @@ export class FormComponent {
   public isLinear: boolean = true;
   public quoteModel: QuoteItems = new QuoteItems();
   public total: number = 0;
+  public tiles = [
+    {
+      type: 'tiles',
+      code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
+      price: 34.12,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '6de173e3-b387-4dee-bbe8-846390719e56',
+      price: 31.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
+      price: 17.25,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
+      price: 19.95,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
+      price: 45.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
+      price: 47.65,
+    } as UnitModel,
+
+    {
+      type: 'tiles',
+      code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
+      price: 34.12,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '6de173e3-b387-4dee-bbe8-846390719e56',
+      price: 31.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
+      price: 17.25,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
+      price: 19.95,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
+      price: 45.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
+      price: 47.65,
+    } as UnitModel,
+
+    {
+      type: 'tiles',
+      code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
+      price: 34.12,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '6de173e3-b387-4dee-bbe8-846390719e56',
+      price: 31.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
+      price: 17.25,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
+      price: 19.95,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
+      price: 45.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
+      price: 47.65,
+    } as UnitModel,
+
+    {
+      type: 'tiles',
+      code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
+      price: 34.12,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '6de173e3-b387-4dee-bbe8-846390719e56',
+      price: 31.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
+      price: 17.25,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
+      price: 19.95,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
+      price: 45.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
+      price: 47.65,
+    } as UnitModel,
+
+    {
+      type: 'tiles',
+      code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
+      price: 34.12,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '6de173e3-b387-4dee-bbe8-846390719e56',
+      price: 31.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
+      price: 17.25,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
+      price: 19.95,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
+      price: 45.05,
+    } as UnitModel,
+    {
+      type: 'tiles',
+      code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
+      price: 47.65,
+    } as UnitModel,
+  ];
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -285,163 +441,7 @@ export class FormComponent {
   }
 
   public getTilesFloorItems(): UnitModel[] {
-    let tiles: UnitModel[] = [
-      {
-        type: 'tiles',
-        code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
-        price: 34.12,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '6de173e3-b387-4dee-bbe8-846390719e56',
-        price: 31.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
-        price: 17.25,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
-        price: 19.95,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
-        price: 45.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
-        price: 47.65,
-      } as UnitModel,
-
-      {
-        type: 'tiles',
-        code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
-        price: 34.12,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '6de173e3-b387-4dee-bbe8-846390719e56',
-        price: 31.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
-        price: 17.25,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
-        price: 19.95,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
-        price: 45.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
-        price: 47.65,
-      } as UnitModel,
-
-      {
-        type: 'tiles',
-        code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
-        price: 34.12,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '6de173e3-b387-4dee-bbe8-846390719e56',
-        price: 31.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
-        price: 17.25,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
-        price: 19.95,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
-        price: 45.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
-        price: 47.65,
-      } as UnitModel,
-
-      {
-        type: 'tiles',
-        code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
-        price: 34.12,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '6de173e3-b387-4dee-bbe8-846390719e56',
-        price: 31.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
-        price: 17.25,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
-        price: 19.95,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
-        price: 45.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
-        price: 47.65,
-      } as UnitModel,
-
-      {
-        type: 'tiles',
-        code: '6b22e6fe-8e2e-4109-8909-7451cf68ed96',
-        price: 34.12,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '6de173e3-b387-4dee-bbe8-846390719e56',
-        price: 31.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '8ddb9d62-d597-4200-899d-2dabd88a43e4',
-        price: 17.25,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: '88a5e4a8-cefa-42f8-adc5-7953df987f66',
-        price: 19.95,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b5ff5a23-c9c0-4895-9fef-96dde9eab5b3',
-        price: 45.05,
-      } as UnitModel,
-      {
-        type: 'tiles',
-        code: 'b7b9476f-b846-49a5-94ca-a60fb76a0d0f',
-        price: 47.65,
-      } as UnitModel,
-    ];
-    return tiles;
+    return this.tiles;
   }
 
   public scroll(id: string): void {
@@ -480,14 +480,15 @@ export class FormComponent {
 
   public setTile(tile: UnitModel): void {
     console.log('hit');
-    /*
+    alert(tile.code);
+
     if (tile.type == 'tiles' || tile.type == 'lvt') {
       this.quoteModel.floorPrice = tile.price;
       this.quoteModel.floorType = tile.code;
       this.quoteModel.floorCode = tile.type;
       //todo: highlite tile
       this.calculateTotal();
-    }*/
+    }
 
     //lvt
     //wall
