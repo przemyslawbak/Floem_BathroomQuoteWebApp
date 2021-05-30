b127ace859d433e6ccbcf7f8d6e46a7069412bb4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-30 18:54:03 +0200

    Sample endpoin added; Backend project relations added;

--- Floem_BathroomQuote/Controllers/QuoteController.cs
+++ Floem_BathroomQuote/Controllers/QuoteController.cs
@@ -1,8 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Floem_Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Floem_BathroomQuote.Controllers
 {
     public class QuoteController : Controller
     {
+        /// <summary>
+        /// POST: api/quote/save
+        /// </summary>
+        /// <returns>Status code.</returns>
+        [HttpPost("save")]
+        public IActionResult SaveQuote([FromBody] FloemQuoteModel model)
+        {
+            return Ok();
+        }
     }
 }
