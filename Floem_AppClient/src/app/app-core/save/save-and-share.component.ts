861eb20dec80beb95359a8c87939f119e3344389
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-05-31 13:28:27 +0200

    Working on save-and-share component;

--- Floem_AppClient/src/app/app-core/save/save-and-share.component.ts
+++ Floem_AppClient/src/app/app-core/save/save-and-share.component.ts
@@ -1,13 +1,39 @@
 import { QuoteService } from './../_services/quote.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from '@services/http.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './save-and-share.component.html',
   styleUrls: ['./save-and-share.component.css'],
 })
-export class SaveAndShareComponent {
-  constructor(private http: HttpService, private quotes: QuoteService) {}
+export class SaveAndShareComponent implements OnInit {
+  public form: FormGroup;
+
+  constructor(
+    private http: HttpService,
+    private quotes: QuoteService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  public ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.form = this.formBuilder.group({
+      Email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  public hasError(name: string) {
+    const e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
+  }
+
+  private getFormControl(name: string) {
+    return this.form.get(name);
+  }
 
   public bookQuoteOnline(): void {
     this.http.getTest('666').subscribe(async (text: string) => {
@@ -19,5 +45,10 @@ export class SaveAndShareComponent {
     this.http.postSaveQuote(this.quotes.quoteState).subscribe(() => {
       //todo
     });
+  }
+
+  public onSubmit() {
+    const email = this.form.value.Email;
+    //todo: send via http
   }
 }
