8af57d09e8d004af9e426094c17fcd15ddba21b4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-06-07 19:02:22 +0200

    Working pon admin pannel;

--- Floem_AppClient/src/app/app-core/admin/admin.component.ts
+++ Floem_AppClient/src/app/app-core/admin/admin.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminModel } from '@models/admin.model';
 import { AdminService } from '@services/admin.service';
+import { HttpService } from '@services/http.service';
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   templateUrl: './admin.component.html',
@@ -9,10 +13,46 @@ import { AdminService } from '@services/admin.service';
 export class AdminComponent implements OnInit {
   public form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private admin: AdminService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    public admin: AdminService,
+    private http: HttpService
+  ) {
+    this.getAdminModel().subscribe((res) => {
+      if (res) {
+        this.admin.adminModel = res;
+      } else {
+        //??
+      }
+    });
+  }
+
+  public getAdminModel(): Observable<AdminModel> {
+    let subject = new Subject<AdminModel>();
+    this.http.getAdminModel().subscribe({
+      next: (a) => {
+        subject.next(a);
+      },
+      error: (e) => {
+        subject.next(null);
+      },
+      complete: () => {},
+    });
+
+    return subject.asObservable();
+  }
 
   public ngOnInit() {
     this.createForm();
+  }
+
+  public hasError(name: string) {
+    const e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
+  }
+
+  private getFormControl(name: string) {
+    return this.form.get(name);
   }
 
   private createForm() {
