36a1503c82f02e2306d982cc9199488f23d6dc03
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-06-07 18:52:31 +0200

    Working on admin view;

--- Floem_AppClient/src/app/app-core/admin/admin.component.ts
+++ Floem_AppClient/src/app/app-core/admin/admin.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminService } from '@services/admin.service';
 
 @Component({
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
 })
-export class AdminComponent {
-  constructor() {}
+export class AdminComponent implements OnInit {
+  public form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private admin: AdminService) {}
+
+  public ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.form = this.formBuilder.group({
+      MirrorInstallation: [
+        this.admin.adminModel.mirrorInstallationPrice,
+        [Validators.required],
+      ],
+    });
+  }
+
+  public onSubmit(): void {
+    //todo: send post request;
+    //todo: update admin service
+  }
 }
